445c79eefb5fd5a0c4f1b36551e2ab8fac80afe8
Christoffer Karlsson <christofferkarlsson1985@gmail.com>
2017-06-14 19:30:46 +0200

    Initial commit

---
+++ Vakt.Api/Models/Nightclub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vakt.Api.Models
+{
+    public class Nightclub
+    {
+        public string NightclubName { get; set; }
+        public string NightclubAdress { get; set; }
+        public string NightclubCity { get; set; }
+        public string NightclubPhoneNumber { get; set; }
+        public string OwnerFirstName { get; set; }
+        public string OwnerLastName { get; set; }
+        public string OwnerPhoneNumber { get; set; }
+    }
+}
---
+++ Vakt.Api/Models/NightclubRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vakt.Api.Models
+{
+   public interface INightclubRepository
+    {
+        IEnumerable<Nightclub> GetAll();
+    }
+
+    public class InMemoryNightClubRepository : INightclubRepository
+    {
+        private List<Nightclub> nightclubs;
+
+        public InMemoryNightClubRepository()
+        {
+            nightclubs = new List<Nightclub>
+            {
+                new Nightclub {NightclubName="Hogges", NightclubAdress="Storgatan", NightclubCity="Hudiksvall", NightclubPhoneNumber="0650-12345", OwnerFirstName="Hogge", OwnerLastName="Strandell", OwnerPhoneNumber="0650-12345"},
+                new Nightclub {NightclubName="Olearys", NightclubAdress="Storgatan", NightclubCity="Hudiksvall", NightclubPhoneNumber="0650-12345", OwnerFirstName="Elvan", OwnerLastName="Anon", OwnerPhoneNumber="0650-12345"},
+                new Nightclub {NightclubName="Sigges", NightclubAdress="Hamnplan 1", NightclubCity="Hudiksvall", NightclubPhoneNumber="0650-12345", OwnerFirstName="Sigge", OwnerLastName="Wassberg", OwnerPhoneNumber="0650-12345"},
+            };
+        }
+
+        public IEnumerable<Nightclub> GetAll()
+        {
+            return nightclubs;
+        }
+    }
+}
